937c633c76ca01b2bb1a7eecaaa65f3db7bd8ce3
pabloFuente <pablofuenteperez@gmail.com>
2018-11-22 17:46:40 +0100

    R command secure scaleFactor value (not 0)

--- selenium-test/src/test/resources/generateGraphs.R
+++ selenium-test/src/test/resources/generateGraphs.R
@@ -14,24 +14,36 @@ savePng <- function(filename, drawnPlot){
 
 # Subscriber RTT
 scaleFactor = max(subscriberData$rtt) / max(subscriberData$browsers)
+if (scaleFactor == 0) {
+	scaleFactor = 1;
+}
 legendPosition = c(0.11, 0.91)
 plot = ggplot(subscriberData, aes(x=time)) + xlab("Time (seconds)") + geom_line(aes(y=rtt, colour="RTT"), size=1.1) + ylab("RTT (milliseconds)") + geom_line(aes(y=browsers*scaleFactor, colour="Participants"), size=1.1) + scale_y_continuous(sec.axis = sec_axis(~./scaleFactor, name = "Number of participants")) + theme(legend.position = legendPosition)
 savePng("subscriberRTT", plot)
 
 # Subscriber Bitrate
 scaleFactor = max(subscriberData$bitrate) / max(subscriberData$browsers)
+if (scaleFactor == 0) {
+	scaleFactor = 1;
+}
 legendPosition = c(0.87, 0.15)
 plot = ggplot(subscriberData, aes(x=time)) + xlab("Time (seconds)") + geom_line(aes(y=bitrate, colour="Bitrate"), size=1.1) + ylab("Bitrate (kbps)") + geom_line(aes(y=browsers*scaleFactor, colour="Participants"), size=1.1) + scale_y_continuous(sec.axis = sec_axis(~./scaleFactor, name = "Number of participants")) + theme(legend.position = legendPosition)
 savePng("subscriberBITRATE", plot)
 
 # Subscriber Jitter
 scaleFactor = max(subscriberData$jitter) / max(subscriberData$browsers)
+if (scaleFactor == 0) {
+	scaleFactor = 1;
+}
 legendPosition = c(0.11, 0.91)
 plot = ggplot(subscriberData, aes(x=time)) + xlab("Time (seconds)") + geom_line(aes(y=jitter, colour="Jitter"), size=1.1) + ylab("Jitter") + geom_line(aes(y=browsers*scaleFactor, colour="Participants"), size=1.1) + scale_y_continuous(sec.axis = sec_axis(~./scaleFactor, name = "Number of participants")) + theme(legend.position = legendPosition)
 savePng("subscriberJITTER", plot)
 
 # Subscriber Delay
 scaleFactor = max(subscriberData$delay) / max(subscriberData$browsers)
+if (scaleFactor == 0) {
+	scaleFactor = 1;
+}
 legendPosition = c(0.11, 0.91)
 plot = ggplot(subscriberData, aes(x=time)) + xlab("Time (seconds)") + geom_line(aes(y=delay, colour="Delay"), size=1.1) + ylab("Delay (milliseconds") + geom_line(aes(y=browsers*scaleFactor, colour="Participants"), size=1.1) + scale_y_continuous(sec.axis = sec_axis(~./scaleFactor, name = "Number of participants")) + theme(legend.position = legendPosition)
 savePng("subscriberDELAY", plot)
@@ -47,6 +59,9 @@ savePng("publisherRTT", plot)
 
 # Publisher Bitrate
 scaleFactor = max(publisherData$bitrate) / max(publisherData$browsers)
+if (scaleFactor == 0) {
+	scaleFactor = 1;
+}
 legendPosition = c(0.11, 0.91)
 plot = ggplot(publisherData, aes(x=time)) + xlab("Time (seconds)") + geom_line(aes(y=bitrate, colour="Bitrate"), size=1.1) + ylab("Bitrate (kbps)") + geom_line(aes(y=browsers*scaleFactor, colour="Participants"), size=1.1) + scale_y_continuous(sec.axis = sec_axis(~./scaleFactor, name = "Number of participants")) + theme(legend.position = legendPosition)
 savePng("publisherBITRATE", plot)
