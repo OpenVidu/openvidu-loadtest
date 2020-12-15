ebf24c905ffd305d6e0e8ff89cd9cde411e60ab0
csantosm <4a.santos@gmail.com>
2020-12-15 11:08:57 +0100

    Added entrypoints
    - Added create PUBLISHER entrypoint
    - Added create SUBSCRIBER entrypoint
    - Added delete streamManager by role
    - Added delete streamManager by uid

--- browser-emulator/src/openvidu-browser/openvidu-browser.ts
+++ browser-emulator/src/openvidu-browser/openvidu-browser.ts
@@ -1,34 +1,34 @@
-import { OpenVidu, Session, StreamEvent } from "openvidu-browser";
-import { OpenViduRole } from 'openvidu-node-client';
+import { OpenVidu, Publisher, Session, StreamEvent } from "openvidu-browser";
+import { OpenViduRole } from "openvidu-node-client";
 import { HttpClient } from "../utils/http-client";
 
 export class OpenViduBrowser {
-	ov: OpenVidu;
-	session: Session;
+	openviduMap: Map<string, OpenVidu> = new Map();
+	sessionMap: Map<string, Session> = new Map();
 	sessionId: string;
 	httpClient: HttpClient;
 
 	constructor() {
 		this.httpClient = new HttpClient();
-		this.ov = new OpenVidu();
-		this.ov.enableProdMode();
-		this.session = this.ov.initSession();
 		this.sessionId = "FAKEwebrtc";
-		this.session.on("streamCreated", (event: StreamEvent) => {
-			this.session.subscribe(event.stream, "subscriber");
-		});
 	}
 
-	async createPublisher(): Promise<void> {
-
+	async createPublisher(uid: string): Promise<string> {
 		return new Promise(async (resolve, reject) => {
-			try {
-				const token: string = await this.getPublisherToken();
-				await this.session.connect(token);
-				var publisher = this.ov.initPublisher(null);
-				await this.session.publish(publisher);
-				resolve();
 
+			const ov = this.createAndStoreOVInstance(uid);
+			const session = this.createAndStoreSessionInstance(uid, ov);
+
+			session.on("streamCreated", (event: StreamEvent) => {
+				session.subscribe(event.stream, null);
+			});
+
+			try {
+				const token: string = await this.getToken(OpenViduRole.PUBLISHER);
+				await session.connect(token);
+				const publisher: Publisher = ov.initPublisher(null);
+				await session.publish(publisher);
+				resolve(uid);
 			} catch (error) {
 				console.log(
 					"There was an error connecting to the session:",
@@ -40,7 +40,67 @@ export class OpenViduBrowser {
 		});
 	}
 
-	private async getPublisherToken(): Promise<string> {
-		return this.httpClient.getToken(this.sessionId, OpenViduRole.PUBLISHER);
+	async createSubscriber(uid: string): Promise<string> {
+		return new Promise(async (resolve, reject) => {
+
+			const ov = this.createAndStoreOVInstance(uid);
+			const session = this.createAndStoreSessionInstance(uid, ov);
+
+			session.on("streamCreated", (event: StreamEvent) => {
+				session.subscribe(event.stream, null);
+			});
+
+			try {
+				const token: string = await this.getToken(OpenViduRole.SUBSCRIBER);
+				await session.connect(token);
+				resolve(uid);
+			} catch (error) {
+				console.log(
+					"There was an error connecting to the session:",
+					error.code,
+					error.message
+				);
+				reject(error);
+			}
+		});
+	}
+
+	deleteStreamManagerWithUid(uid: string) {
+		const session = this.sessionMap.get(uid);
+		session?.disconnect();
+		this.deleteInstancesFromId(uid);
+	}
+
+	deleteStreamManagerWithRole(role: OpenViduRole) {
+		this.sessionMap.forEach((session: Session, uid: string) => {
+			if (session.connection.role === role) {
+				session.disconnect();
+				this.deleteInstancesFromId(uid);
+			}
+		});
+	}
+
+	private async getToken(role: OpenViduRole): Promise<string> {
+		return this.httpClient.getToken(this.sessionId, role);
+	}
+
+	private createAndStoreOVInstance(uid: string): OpenVidu {
+		const ov: OpenVidu = new OpenVidu();
+		ov.enableProdMode();
+		// Store the OV object into a map
+		this.openviduMap.set(uid, ov);
+		return ov;
+	}
+
+	private createAndStoreSessionInstance(uid:string, ov: OpenVidu): Session {
+		const session: Session = ov.initSession();
+		// Store the session object into a map
+		this.sessionMap.set(uid, session);
+		return session;
+	}
+
+	private deleteInstancesFromId(uid: string) {
+		this.sessionMap.delete(uid);
+		this.openviduMap.delete(uid);
 	}
 }
--- browser-emulator/src/server.ts
+++ browser-emulator/src/server.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import { Request, Response } from 'express';
+
 import { SERVER_PORT, OPENVIDU_URL, OPENVIDU_SECRET } from "./config";
 import { Hack } from "./extra/hack";
 import { OpenViduBrowser } from "./openvidu-browser/openvidu-browser";
@@ -6,8 +8,51 @@ import { OpenViduBrowser } from "./openvidu-browser/openvidu-browser";
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+let ovBrowser: OpenViduBrowser;
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+
+app.post("/openvidu-browser/publisher", async (req: Request, res: Response) => {
+	try {
+		console.log("UID", req.body);
+		const uid = req.body.uid;
+		await ovBrowser.createPublisher(uid);
+		res.status(200).send({uid});
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
+});
+
+app.post("/openvidu-browser/subscriber", async (req: Request, res: Response) => {
+	try {
+		const uid = req.body.uid;
+		console.log("uid", uid)
+		await ovBrowser.createSubscriber(uid);
+		res.status(200).send({uid});
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
+});
+
+app.delete("/openvidu-browser/stream", (req: Request, res: Response) => {
+	try {
+		const uid = req.query.uid;
+		const role = req.query.role;
+		if(!!uid) {
+			ovBrowser.deleteStreamManagerWithUid(uid);
+		}else {
+			ovBrowser.deleteStreamManagerWithRole(role);
+		}
+		res.status(200).send({});
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error);
+	}
+});
 
 app.listen(SERVER_PORT, () => {
 	const hack = new Hack();
@@ -15,6 +60,7 @@ app.listen(SERVER_PORT, () => {
 	hack.openviduBrowser();
 	hack.webrtc();
 	hack.websocket();
+	ovBrowser = new OpenViduBrowser();
 
 	console.log("---------------------------------------------------------");
 	console.log(" ");
@@ -23,15 +69,4 @@ app.listen(SERVER_PORT, () => {
 	console.log(`Listening in port ${SERVER_PORT}`);
 	console.log(" ");
 	console.log("---------------------------------------------------------");
-});
-
-app.post("/", async (req, res) => {
-	try {
-		const ovBrowser = new OpenViduBrowser();
-		await ovBrowser.createPublisher();
-		res.status(200).send({});
-	} catch (error) {
-		console.log(error);
-		res.status(500).send(error);
-	}
 });
