520ca2aa5b92646e694ae187abb2f0426a895611
csantosm <4a.santos@gmail.com>
2021-01-05 14:08:38 +0100

    Added programmatic video to publishers
    Added programmatic video with node-canvas

--- browser-emulator/src/openvidu-browser/openvidu-browser.ts
+++ browser-emulator/src/openvidu-browser/openvidu-browser.ts
@@ -1,6 +1,9 @@
 import { OpenVidu, Publisher, Session, StreamEvent } from "openvidu-browser";
 import { OpenViduRole } from "openvidu-node-client";
 import { HttpClient } from "../utils/http-client";
+const { RTCVideoSource, rgbaToI420 } = require('wrtc').nonstandard;
+const { createCanvas, loadImage } = require('canvas');
+const { performance } = require('perf_hooks');
 
 export class OpenViduBrowser {
 	openviduMap: Map<string, OpenVidu> = new Map();
@@ -27,6 +30,7 @@ export class OpenViduBrowser {
 				if(role === OpenViduRole.PUBLISHER){
 					const publisher: Publisher = ov.initPublisher(null);
 					await session.publish(publisher);
+					await this.createVideoCanvas(publisher);
 				}
 
 				this.storeInstances(ov, session);
@@ -75,5 +79,42 @@ export class OpenViduBrowser {
 	private deleteInstancesFromId(connectionId: string) {
 		this.sessionMap.delete(connectionId);
 		this.openviduMap.delete(connectionId);
+	}
+
+	private async createVideoCanvas(publisher: Publisher){
+		const width = publisher.stream.videoDimensions.width;
+		const height = publisher.stream.videoDimensions.height;
+		const source = new RTCVideoSource();
+		const track = source.createTrack();
+		const canvas = createCanvas(width, height);
+		const context = canvas.getContext('2d');
+		const myimg = await loadImage('src/assets/images/openvidu_logo.png');
+		context.fillStyle = 'black';
+		context.fillRect(0, 0, width, height);
+
+		await publisher.replaceTrack(track);
+		const interval = setInterval(() => {
+
+			const thisTime = performance.now();
+
+			context.save();
+			context.translate(width * 0.5, height * 0.5);
+			context.rotate(thisTime / 1000);
+			context.translate(-myimg.width * 0.5, -myimg.height * 0.5);
+			context.fillStyle = 'black';
+			context.fillRect(0, 0, width, height);
+			context.drawImage(myimg,0,0);
+			context.restore();
+
+			const rgbaFrame = context.getImageData(0, 0, width, height);
+			const i420Frame = {
+				width,
+				height,
+				data: new Uint8ClampedArray(1.5 * width * height)
+			};
+			rgbaToI420(rgbaFrame, i420Frame);
+			source.onFrame(i420Frame);
+		});
+
 	}
 }
