dd1c946ca9dd15b235ffb337fcc2b49f84f1da8d
csantosm <4a.santos@gmail.com>
2021-01-11 15:56:29 +0100

    Added load test controller

---
+++ load-test/src/main/java/io/openvidu/loadtest/LoadTestApplication.java
@@ -0,0 +1,13 @@
+package io.openvidu.loadtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadTestApplication.class, args);
+	}
+
+}
---
+++ load-test/src/test/java/io/openvidu/loadtest/LoadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.openvidu.loadtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
