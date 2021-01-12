59b2a7c45d96d8baee697522719d42ac9a12bdd4
csantosm <4a.santos@gmail.com>
2021-01-12 11:42:30 +0100

    Changed json library by GSON

--- load-test/src/main/java/io/openvidu/loadtest/utils/DataIO.java
+++ load-test/src/main/java/io/openvidu/loadtest/utils/DataIO.java
@@ -2,33 +2,56 @@ package io.openvidu.loadtest.utils;
 
 import java.io.File;
 import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 
+import io.openvidu.loadtest.models.testcase.TestCase;
 
 public class DataIO {
-	
+
 	private static ClassLoader classLoader = DataIO.class.getClassLoader();
 	private static final String TEST_CASES_JSON_FILE = "test_cases.json";
 
-	public JSONArray getTestCasesFromJSON() {
+	public List<TestCase> getTestCasesFromJSON() {
 		File file = new File(classLoader.getResource(TEST_CASES_JSON_FILE).getFile());
+		Gson gson = new Gson();
+		JsonArray testCasesList = new JsonArray();
 
-		JSONParser parser = new JSONParser();
-		JSONArray testCasesList = new JSONArray();
-		
 		try {
-			Object obj = parser.parse(new FileReader(file.getAbsolutePath()));
-			JSONObject jsonObject = (JSONObject) obj;
-			testCasesList = (JSONArray) jsonObject.get("testcases");
- 
+			JsonReader reader = new JsonReader(new FileReader(file.getAbsolutePath()));
+			JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+			testCasesList = (JsonArray) jsonObject.get("testcases");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		return testCasesList;
+
+		return this.convertJsonArrayToTestCasesList(testCasesList);
 	}
-	
+
+	private List<TestCase> convertJsonArrayToTestCasesList(JsonArray array) {
+
+		Type listType = new TypeToken<List<String>>() {}.getType();
+		List<TestCase> testCaseList = new ArrayList<TestCase>();
+
+		for (int i = 0; i < array.size(); i++) {
+			String typology = array.get(i).getAsJsonObject().get("typology").getAsString();
+			String sessions = array.get(i).getAsJsonObject().get("sessions").getAsString();
+			JsonArray participantsArray = (JsonArray) array.get(i).getAsJsonObject().get("participants");
+			List<String> participants = new Gson().fromJson(participantsArray, listType);
+
+			testCaseList.add(new TestCase(typology, participants, sessions));
+		}
+
+		return testCaseList;
+
+	}
+
 }
