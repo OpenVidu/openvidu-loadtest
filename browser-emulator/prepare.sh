b6ef64f8a8c08c92bedd0d65a8155fa5a0241077
csantosm <4a.santos@gmail.com>
2021-03-01 13:51:06 +0100

    browser-emulator: Updated prepare script

--- browser-emulator/prepare.sh
+++ browser-emulator/prepare.sh
@@ -2,12 +2,19 @@
 
 # Install third party libraries required
 sudo apt-get update
+
+# Install Node.js
 sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt-get install -y nodejs
+
+# Install Docker CE
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
 sudo apt install -y docker-ce
 sudo usermod -aG docker $USER
-newgrp docker
+sudo newgrp docker
+
+# Install browser-emulator dependencies
+npm install
