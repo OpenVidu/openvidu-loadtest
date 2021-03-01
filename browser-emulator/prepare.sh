f81c1709bb7f7d1a2b459dca5ff58c0d874f5e89
csantosm <4a.santos@gmail.com>
2021-03-01 11:44:31 +0100

    browser-emulator: Fixed prepare script

--- browser-emulator/prepare.sh
+++ browser-emulator/prepare.sh
@@ -4,4 +4,10 @@
 sudo apt-get update
 sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt-get install -y nodejs
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker $USER
+newgrp docker
