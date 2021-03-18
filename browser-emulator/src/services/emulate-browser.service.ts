7802ff5919492d6400681e04c298f7d16fec1b0c
csantosm <4a.santos@gmail.com>
2021-03-18 13:41:10 +0100

    browser-emulator: Created a MediaStreamTrack from a video file
    Created a MediaStreamTrack from a video file using ffmpeg for inject it to the node-webrtc library instead of canvas

--- browser-emulator/src/services/emulate-browser.service.ts
+++ browser-emulator/src/services/emulate-browser.service.ts
@@ -2,31 +2,33 @@ import { OpenVidu, Publisher, Session, StreamEvent } from "openvidu-browser";
 import { HttpClient } from "../utils/http-client";
 import { OpenViduRole } from '../types/openvidu.type';
 import { TestProperties } from '../types/api-rest.type';
-const { RTCVideoSource, RTCAudioSource, rgbaToI420 } = require('wrtc').nonstandard;
+const { RTCVideoSource, RTCAudioSource } = require('wrtc').nonstandard;
 import { Canvas, createCanvas, Image, loadImage } from 'canvas';
 import { EMULATED_USER_TYPE } from '../config';
 import { EmulatedUserType } from '../types/config.type';
+
+const ffmpeg = require('fluent-ffmpeg');
+const chunker = require('stream-chunker');
+import { StreamOutput } from 'fluent-ffmpeg-multistream';
+
+
+interface CustomMediaStream {
+	url: string,
+	track: MediaStreamTrack,
+	options: string[],
+	kind: string,
+	width?: number,
+	height?:number
+};
 
 export class EmulateBrowserService {
 	private openviduMap: Map<string, {openvidu: OpenVidu, session: Session, audioTrackInterval: NodeJS.Timer}> = new Map();
 	private readonly WIDTH = 640;
 	private readonly HEIGHT = 480;
-	private readonly MIN_FREQUENCY = 500;
-	private readonly MAX_FREQUENCY = 1000;
-	private videoSource;
-	private videoTrack: MediaStreamTrack | boolean = true;
-	private canvas: Canvas;
-	private context;
-	private myimg: Image;
-	private MIN = 0;
-	private canvasInterval: NodeJS.Timer;
-	private CANVAS_MAX_HEIGHT: number;
-	private CANVAS_MAX_WIDTH: number;
-
+	private videoTrack: MediaStreamTrack | boolean;
+	private audioTrack: MediaStreamTrack | boolean;
+	private mediaStramTracksCreated: boolean = false;
 	constructor(private httpClient: HttpClient = new HttpClient()) {
-		if(this.isUsingNodeWebrtc()) {
-			this.initializeVideoCanvas();
-		}
 	}
 
 	async createStreamManager(token: string, properties: TestProperties): Promise<string> {
@@ -47,33 +49,19 @@ export class EmulateBrowserService {
 
 				await session.connect(token,  properties.userId);
 				if(properties.role === OpenViduRole.PUBLISHER){
-					let audioSource: MediaStreamTrack | boolean = properties.audio;
-					if(this.isUsingNodeWebrtc()) {
-						this.stopVideoCanvasInterval();
-					}
 
-					// TODO: Debugear openvidu para ver que pasa con el audio track
-
-					if(audioSource) {
-						//Create an emulated audio stream track
-						const audioObject = this.initializeAudio();
-						audioSource = audioObject.audioSource;
-						// audioTrackInterval = audioObject.audioTrackInterval;
-					}
+					await this.createMediaStreamTracks(properties);
 
 					const publisher: Publisher = ov.initPublisher(null, {
-						audioSource: audioSource,
-						videoSource: properties.video ? this.videoTrack : null,
+						audioSource: this.audioTrack,
+						videoSource: this.videoTrack,
 						publishAudio: properties.audio,
 						publishVideo: properties.video,
 						resolution: this.WIDTH + 'x' + this.HEIGHT,
 						frameRate: properties.frameRate,
 					});
 					await session.publish(publisher);
-					if(this.isUsingNodeWebrtc()) {
-						const frameRateEstimation = 1000 / properties.frameRate;
-						this.startVideoCanvasInterval(frameRateEstimation);
-					}
+
 				}
 
 				this.storeInstances(ov, session, audioTrackInterval);
@@ -119,85 +107,106 @@ export class EmulateBrowserService {
 		this.openviduMap.set(session.connection.connectionId, {openvidu, session, audioTrackInterval});
 	}
 
-	private async startVideoCanvasInterval(timeoutMs: number = 30) {
-		// Max 30 fps
-		timeoutMs =  timeoutMs < 30 ? 30 : timeoutMs;
-		this.context.fillStyle = '#' + Math.floor(Math.random()*16777215).toString(16);
+	private async createMediaStreamTracks(properties: TestProperties): Promise<void> {
 
-		this.canvasInterval = setInterval(() => {
-			const x = Math.floor(Math.random() * (this.CANVAS_MAX_WIDTH - this.MIN + 1) + this.MIN);
-			const y = Math.floor(Math.random() * (this.CANVAS_MAX_HEIGHT - this.MIN + 1) + this.MIN);
+		if(!this.mediaStramTracksCreated) {
 
-			this.context.save();
-			this.context.fillRect(0, 0, this.WIDTH, this.HEIGHT);
-			this.context.drawImage(this.myimg, x, y);
-			// this.context.font = "50px Verdana";
-			// this.context.strokeText("Hello World!", 100, 100);
+			this.videoTrack = properties.video;
+			this.audioTrack = properties.audio;
 
-			this.context.restore();
+			if(this.isUsingNodeWebrtc()) {
+				if(properties.audio || properties.video) {
+					await this.createMediaStreamTracksFromVideoFile(properties.video, properties.audio);
+				}
+			}
+		}
 
-			const rgbaFrame = this.context.getImageData(0, 0, this.WIDTH, this.HEIGHT);
-			const i420Frame = {
+	}
+
+	private async createMediaStreamTracksFromVideoFile(video: boolean, audio: boolean) {
+
+		let videoOutput = null;
+		let audioOutput = null;
+
+		const command = ffmpeg()
+							.input(`${process.env.PWD}/src/assets/mediafiles/video.mp4`)
+							.inputOptions(['-stream_loop -1', '-r 1'])
+
+		if(video) {
+			videoOutput = this.createVideoOutput();
+			command
+				.output(videoOutput.url)
+				.outputOptions(videoOutput.options)
+		}
+
+		if(audio) {
+			audioOutput = this.createAudioOutput();
+			command
+				.output(audioOutput.url)
+				.outputOptions(audioOutput.options)
+		}
+
+		command.run();
+
+		this.videoTrack = videoOutput.track;
+		this.audioTrack = audioOutput.track;
+		this.mediaStramTracksCreated = true;
+	}
+
+	private createVideoOutput(): CustomMediaStream {
+
+		const ws = chunker(this.WIDTH * this.HEIGHT * 1.5);
+		const source = new RTCVideoSource();
+		const ffmpegOptions = [
+			'-f rawvideo',
+			// '-c:v rawvideo',
+			`-s ${this.WIDTH}x${this.HEIGHT}`,
+			'-pix_fmt yuv420p',
+			'-r 24'
+		];
+
+		ws.on('data', (chunk) => {
+			const data = {
 				width: this.WIDTH,
 				height: this.HEIGHT,
-				data: new Uint8ClampedArray(1.5 * this.WIDTH * this.HEIGHT)
+				data: new Uint8ClampedArray(chunk)
 			};
-			rgbaToI420(rgbaFrame, i420Frame);
-			this.videoSource.onFrame(i420Frame);
-		}, timeoutMs);
+			source.onFrame(data);
+		});
+
+		const output = StreamOutput(ws);
+		output.track = source.createTrack();
+		output.options = ffmpegOptions;
+		output.kind = 'video';
+		output.width = this.WIDTH;
+		output.height = this.HEIGHT;
+
+		return output;
 	}
+	private createAudioOutput(): CustomMediaStream {
+		const sampleRate = 48000;
+		const ws = chunker(2 * sampleRate / 100)
+		const source = new RTCAudioSource();
+		const ffmpegOptions = [
+			'-f s16le',
+			'-ar 48k',
+			'-ac 1'
+		];
+		ws.on('data', (chunk) => {
+			const data = {
+				samples: new Int16Array(chunk.buffer.slice(chunk.byteOffset, chunk.byteOffset + chunk.length)),
+				sampleRate
+			};
 
-	private stopVideoCanvasInterval() {
-		clearInterval(this.canvasInterval);
-	}
+			source.onData(data);
+		});
 
-	private async initializeVideoCanvas(){
-		this.videoSource = new RTCVideoSource();
-		this.videoTrack = this.videoSource.createTrack();
-		this.canvas = createCanvas(this.WIDTH, this.HEIGHT);
-		this.context = this.canvas.getContext('2d');
-		this.myimg = await loadImage('src/assets/images/openvidu_logo.png');
-		this.context.fillStyle = 'black';
-		this.context.fillRect(0, 0, this.WIDTH, this.HEIGHT);
-		this.CANVAS_MAX_WIDTH = this.WIDTH - this.myimg.width;
-		this.CANVAS_MAX_HEIGHT = this.HEIGHT - this.myimg.height;
-		// Try with a video file https://dev.to/benjaminadk/encode-gifs-with-node-4c75
-	}
+		const output = StreamOutput(ws)
+		output.track = source.createTrack()
+		output.options = ffmpegOptions;
+		output.kind = 'audio';
 
-	private initializeAudio(): {audioSource:MediaStreamTrack, audioTrackInterval: NodeJS.Timer} {
-
-		const options = {
-			channelCount: 1,
-			sampleRate: 30000,
-			frequency: Math.floor(Math.random() * (this.MAX_FREQUENCY - this.MIN_FREQUENCY) + this.MIN_FREQUENCY),
-			time: 0,
-		};
-		const bitsPerSample = 16;
-		const maxValue = Math.pow(2, bitsPerSample) / 2 - 1;
-		const numberOfFrames = options.sampleRate / 100;
-		const secondsPerSample = 1 / options.sampleRate;
-		const audioSource = new RTCAudioSource();
-		const samples = new Int16Array(options.channelCount * numberOfFrames);
-
-		const data = {
-			samples,
-			sampleRate: options.sampleRate,
-			bitsPerSample,
-			channelCount: options.channelCount,
-			numberOfFrames
-		};
-
-		const audioInterval = setInterval(() => {
-			for (let i = 0; i < numberOfFrames; i++) {
-				options.time += secondsPerSample;
-				for (let j = 0; j < options.channelCount; j++) {
-				  samples[i * options.channelCount + j] = Math.sin(2 * Math.PI * options.frequency * options.time) * maxValue;
-				}
-			  }
-			  audioSource.onData(data);
-		}, 50)
-
-		return {audioSource: audioSource.createTrack(), audioTrackInterval: audioInterval};
+		return output;
 	}
 
 	private isUsingNodeWebrtc(): boolean {
