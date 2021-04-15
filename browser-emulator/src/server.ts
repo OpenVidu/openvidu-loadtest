932b467597bf5c3ea544c38785bd771b00f7a568
csantosm <4a.santos@gmail.com>
2021-04-15 17:49:39 +0200

    browser-emulator: Allowed all rest origins

--- browser-emulator/src/server.ts
+++ browser-emulator/src/server.ts
@@ -21,6 +21,14 @@ const options = {
 	cert: fs.readFileSync('public/cert.pem', 'utf8'),
 };
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept',
+	);
+	next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
