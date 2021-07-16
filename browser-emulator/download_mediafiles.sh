6c5470fe8fb6a475b09b7a2c26c56b6221d6c5ff
Juan Navarro <juan.navarro@gmx.es>
2021-07-16 14:06:02 +0200

    browser-emulator: download_mediafiles.sh: allow resuming downloads
    Use --continue-at so downloads can be resumed.
    Also use --location which is a good practice in curl, to follow HTTP
    redirections in case there is any.

--- browser-emulator/download_mediafiles.sh
+++ browser-emulator/download_mediafiles.sh
@@ -23,26 +23,31 @@ mkdir -p "$MEDIAFILES_DIR"
 # Mediafiles for Chrome
 if [[ ! -f "$MEDIAFILES_DIR/fakevideo_1280x720.y4m" ]]; then
     curl --output "$MEDIAFILES_DIR/fakevideo_1280x720.y4m" \
-        "https://s3.eu-west-1.amazonaws.com/public.openvidu.io/bbb_1280x720.y4m"
+        --continue-at - \
+        --location "https://s3.eu-west-1.amazonaws.com/public.openvidu.io/bbb_1280x720.y4m"
 fi
 if [[ ! -f "$MEDIAFILES_DIR/fakevideo_640x480.y4m" ]]; then
     curl --output "$MEDIAFILES_DIR/fakevideo_640x480.y4m" \
-    "https://s3.eu-west-1.amazonaws.com/public.openvidu.io/bbb_640x480.y4m"
+        --continue-at - \
+        --location "https://s3.eu-west-1.amazonaws.com/public.openvidu.io/bbb_640x480.y4m"
 fi
 if [[ ! -f "$MEDIAFILES_DIR/fakeaudio.wav" ]]; then
     curl --output "$MEDIAFILES_DIR/fakeaudio.wav" \
-        "https://s3-eu-west-1.amazonaws.com/public.openvidu.io/bbb.wav"
+        --continue-at - \
+        --location "https://s3-eu-west-1.amazonaws.com/public.openvidu.io/bbb.wav"
 fi
 
 # Mediafiles for KMS
 if [[ ! -f "$MEDIAFILES_DIR/video_640x480.mkv" ]]; then
     curl --output "$MEDIAFILES_DIR/video_640x480.mkv" \
-        "https://s3.eu-west-1.amazonaws.com/public.openvidu.io/bbb_640x480.mkv"
+        --continue-at - \
+        --location "https://s3.eu-west-1.amazonaws.com/public.openvidu.io/bbb_640x480.mkv"
     #"https://s3-eu-west-1.amazonaws.com/public.openvidu.io/fakevideo_vp8_opus.mkv"
     # https://s3-eu-west-1.amazonaws.com/public.openvidu.io/fakevideo_h264_opus.mkv
 fi
 if [[ ! -f "$MEDIAFILES_DIR/video_1280x720.mkv" ]]; then
     curl --output "$MEDIAFILES_DIR/video_1280x720.mkv" \
-        "https://s3.eu-west-1.amazonaws.com/public.openvidu.io/bbb_1280x720.mkv"
+        --continue-at - \
+        --location "https://s3.eu-west-1.amazonaws.com/public.openvidu.io/bbb_1280x720.mkv"
     # https://s3-eu-west-1.amazonaws.com/public.openvidu.io/fakevideo_h264_opus.mkv
 fi
