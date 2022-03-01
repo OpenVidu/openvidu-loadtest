a502dff3ccc5c1a8eb0edb0321f8444492bacd69
ivchicano <46921761+ivchicano@users.noreply.github.com>
2022-03-01 13:14:05 +0100

    browser-emulator: qoe controller fix wrong recordings path

--- browser-emulator/src/controllers/qoe.controller.ts
+++ browser-emulator/src/controllers/qoe.controller.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import multer = require('multer');
 import { Request, Response } from 'express';
 
-const RECORDINGS_PATH = `${process.env.PWD}/qoe`;
+const RECORDINGS_PATH = `${process.env.PWD}/recordings/qoe`;
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, RECORDINGS_PATH)
