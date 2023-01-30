ff3c9cb5362ae33a18ed35df39042d7e8f701ead
ivchicano <ivchicano@gmail.com>
2023-01-30 11:38:00 +0000

    browser-emulator: fix audio and video desync on real browsers

--- browser-emulator/recording_scripts/start-fake-media.sh
+++ browser-emulator/recording_scripts/start-fake-media.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export FFREPORT=file=/opt/openvidu-loadtest/browser-emulator/ffmpeg_logs/ffmpeg-$(date +%Y%m%s).log:level=24
+export FFREPORT=file=/opt/openvidu-loadtest/browser-emulator/ffmpeg_logs/ffmpeg-$(date +%Y%m%s).log:level=56
 ffmpeg -y \
     -stream_loop -1 -re -i $1 \
-    -stream_loop -1 -re -i $2 \
+    -stream_loop -1 -re -itsoffset 1 -i $2 \
     -map 0 \
-    -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 -shortest /dev/video0 \
+    -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 \
     -map 1 \
-    -f s16le -ar 48000 -ac 2 -shortest /tmp/virtmic > /dev/null 2>&1 < /dev/null > /dev/null 2>&1 < /dev/null
+    -f s16le -ar 48000 -ac 2 -threads 0 /tmp/virtmic > /dev/null 2>&1 < /dev/null > /dev/null 2>&1 < /dev/null
