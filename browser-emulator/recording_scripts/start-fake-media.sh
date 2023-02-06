30e3f9f0588a54ccb29fcd07ebf0bba567a4e7df
ivchicano <ivchicano@gmail.com>
2023-02-06 13:07:42 +0000

    browser-emulator: clean start-fake-media code

--- browser-emulator/recording_scripts/start-fake-media.sh
+++ browser-emulator/recording_scripts/start-fake-media.sh
@@ -6,4 +6,4 @@ ffmpeg -y \
     -map 0 \
     -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 \
     -map 1 \
-    -f s16le -ar 48000 -ac 2 -threads 0 /tmp/virtmic > /dev/null 2>&1 < /dev/null > /dev/null 2>&1 < /dev/null
+    -f s16le -ar 48000 -ac 2 -threads 0 /tmp/virtmic > /dev/null 2>&1 < /dev/null
