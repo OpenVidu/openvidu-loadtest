887381af67ef999826a43477153b63015065157a
ivchicano <ivchicano@gmail.com>
2023-02-14 14:55:16 +0000

    browser-emulator: remove unnecessary hacks

--- browser-emulator/recording_scripts/start-fake-media.sh
+++ browser-emulator/recording_scripts/start-fake-media.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-export FFREPORT=file=/opt/openvidu-loadtest/browser-emulator/ffmpeg_logs/ffmpeg-$(date +%Y%m%s).log:level=56
 ffmpeg -y \
     -stream_loop -1 -re -i $1 \
-    -stream_loop -1 -re -itsoffset 1 -i $2 \
+    -stream_loop -1 -re -i $2 \
     -map 0 \
     -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 \
     -map 1 \
--- browser-emulator/src/utils/fake-media-devices.ts
+++ browser-emulator/src/utils/fake-media-devices.ts
@@ -25,12 +25,6 @@ export async function startFakeMediaDevices(videoPath: string, audioPath: string
         await runScript(`${process.env.PWD}/recording_scripts/start-fake-media.sh ${videoPath} ${audioPath}`, {
             detached: true
         });
-
-        // This hack forces the audio and video stream to start before the browser is open, acting as a working sink to avoiding the browser not detecting the media devices
-        await runScript(`ffmpeg -i /tmp/virtmic -f s16le -ar 48000 -ac 2 -`, {
-            detached: true,
-            redirectStdoutToFile: "/dev/null"
-        })
         console.log("Fake webcam started.");
         console.log("Fake microphone started.");
     }
