268e6d3e62e3497a56d49b814eeba3776de5b085
ivchicano <46921761+ivchicano@users.noreply.github.com>
2023-07-17 12:03:55 +0200

    loadtest-controller: add cpu and mem monitoring to debug_vnc script

--- loadtest-controller/debug_vnc.sh
+++ loadtest-controller/debug_vnc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-apt-get install -y x11vnc net-tools
+sed '0,\/dev\/null/{s//var\/log\/ffmpeg.log/}' /opt/openvidu-loadtest/browser-emulator/recording_scripts/start-fake-media.sh > /tmp/tmpfile && mv /tmp/tmpfile /opt/openvidu-loadtest/browser-emulator/recording_scripts/start-fake-media.sh
+chmod +x /opt/openvidu-loadtest/browser-emulator/recording_scripts/start-fake-media.sh
+apt-get install -y x11vnc net-tools sysstat
 # Credit to https://gist.github.com/tullmann/476cc71169295d5c3fe6
 # Wait for X Server to be ready
 echo "Xvfb :10 -screen 0 1920x1080x24 -ac &
@@ -22,5 +24,8 @@ echo \"X is available\"
 x11vnc -display :10 -forever -shared -bg -nopw" > /opt/openvidu-loadtest/browser-emulator/x11vnc_script.sh
 chmod +x /opt/openvidu-loadtest/browser-emulator/x11vnc_script.sh
 (crontab -u ubuntu -l ; echo '@reboot cd /opt/openvidu-loadtest/browser-emulator && ./x11vnc_script.sh > /var/log/x11vnc.log 2>&1') | crontab -u ubuntu -
-sed '0,\/dev\/null/{s//var\/log\/ffmpeg.log/}' /opt/openvidu-loadtest/browser-emulator/recording_scripts/start-fake-media.sh > /tmp/tmpfile && mv /tmp/tmpfile /opt/openvidu-loadtest/browser-emulator/recording_scripts/start-fake-media.sh
-chmod +x /opt/openvidu-loadtest/browser-emulator/recording_scripts/start-fake-media.sh
+(crontab -u ubuntu -l ; echo '@reboot sar -o /var/log/sar_cpu.log 1 > /dev/null 2>&1') | crontab -u ubuntu -
+(crontab -u ubuntu -l ; echo '@reboot sar -o /var/log/sar_mem.log -r 1 > /dev/null 2>&1') | crontab -u ubuntu -
+x11vnc -display :10 -forever -shared -bg -nopw > /var/log/x11vnc.log 2>&1
+sar -o /var/log/sar_cpu.log 1 > /dev/null 2>&1 &
+sar -o /var/log/sar_mem.log -r 1 > /dev/null 2>&1 &
