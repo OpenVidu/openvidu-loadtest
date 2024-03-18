da630a59b3ba3e2abbcd0c32265747e3a920dd39
ivchicano <ivchicano@gmail.com>
2024-03-18 13:55:26 +0100

    browser-emulator: fix notify-aws cfn-signal path

--- browser-emulator/notify-aws.sh
+++ browser-emulator/notify-aws.sh
@@ -10,6 +10,6 @@ set -o xtrace
 pip3 install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-latest.tar.gz
 
 # sending the finish call
-cfn-signal -e 0 --stack ${AWS_STACK} --resource BrowserInstance --region ${AWS_REGION}
+/usr/local/bin/cfn-signal -e 0 --stack ${AWS_STACK} --resource BrowserInstance --region ${AWS_REGION}
 
 echo "Instance is ready"
