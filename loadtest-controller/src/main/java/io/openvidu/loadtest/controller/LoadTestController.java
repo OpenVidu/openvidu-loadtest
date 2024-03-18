92f9f5a2b1ab7b89e3a021969daf3b82255c7682
ivchicano <ivchicano@gmail.com>
2024-03-18 18:12:38 +0100

    loadtest-controller: fix test not stopping in rare cases

--- loadtest-controller/src/main/java/io/openvidu/loadtest/controller/LoadTestController.java
+++ loadtest-controller/src/main/java/io/openvidu/loadtest/controller/LoadTestController.java
@@ -644,15 +644,14 @@ public class LoadTestController {
 
 	private CreateParticipantResponse getLastResponse(List<Future<CreateParticipantResponse>> futureList) {
 		CreateParticipantResponse lastResponse = null;
-		boolean errored = false;
 		for (Future<CreateParticipantResponse> future : futureList) {
 			try {
 				CreateParticipantResponse futureResponse = future.get();
 				if (!futureResponse.isResponseOk()) {
 					lastResponse = futureResponse;
-					errored = true;
+					break;
 				}
-				if (!errored || (lastResponse == null) || (futureResponse.getStreamsInWorker() >= lastResponse
+				if ((lastResponse == null) || (futureResponse.getStreamsInWorker() >= lastResponse
 						.getStreamsInWorker())) {
 					lastResponse = futureResponse;
 				}
