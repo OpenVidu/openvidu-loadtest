cfa6cdd0b99532b913930c766695225b4e3ad1ff
ivchicano <ivchicano@gmail.com>
2024-04-23 15:23:12 +0200

    browser-emulator: fix vagrant box disk being full

--- browser-emulator/vagrantprovision.sh
+++ browser-emulator/vagrantprovision.sh
@@ -70,22 +70,22 @@ unset HISTFILE
 rm -f /root/.bash_history
 rm -f /home/vagrant/.bash_history
 
-# Whiteout root
-count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`;
-count=$((count -= 1))
-dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count;
-rm /tmp/whitespace;
+# # Whiteout root
+# count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`;
+# count=$((count -= 1))
+# dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count;
+# rm /tmp/whitespace;
 
-# Whiteout /boot
-count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
-count=$((count -= 1))
-dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
-rm /boot/whitespace;
+# # Whiteout /boot
+# count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
+# count=$((count -= 1))
+# dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count;
+# rm /boot/whitespace;
 
-# Whiteout swap 
-swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
-swapoff $swappart;
-dd if=/dev/zero of=$swappart;
-mkswap $swappart;
-swapon $swappart;
+# # Whiteout swap 
+# swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
+# swapoff $swappart;
+# dd if=/dev/zero of=$swappart;
+# mkswap $swappart;
+# swapon $swappart;
 reboot now
