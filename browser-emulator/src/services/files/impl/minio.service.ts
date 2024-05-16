371264faf80e6b5ea748f99fd1d7fcfa1b45f344
ivchicano <ivchicano@gmail.com>
2024-05-16 15:17:26 +0200

    browser-emulator: update minio version

--- browser-emulator/src/services/files/impl/minio.service.ts
+++ browser-emulator/src/services/files/impl/minio.service.ts
@@ -38,36 +38,28 @@ export class MinioFilesService extends FilesService {
         }
 
         const uploadFile = async (fileName: string, filePath: string): Promise<void> => {
-            return new Promise((resolve, reject) => {
-                let randomDelay = Math.floor(Math.random() * (10000 - 0 + 1)) + 0;
-                console.log(`Uploading file ${fileName} to MINIO bucket ${process.env.MINIO_BUCKET} with delay ${randomDelay} ms`);
-                setTimeout(() => {
-                    this.minioClient.fPutObject(process.env.MINIO_BUCKET, fileName, filePath, async (err, etag) => {
-                        if (err) {
-                            if (err.code === 'SlowDown') {
-                                let retryDelay = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000; // Random delay between 5 and 10 seconds
-                                console.warn(`Received SlowDown error. Retrying upload file ${fileName} to MINIO bucket ${process.env.MINIO_BUCKET} after delay ${retryDelay} ms`);
-
-                                setTimeout(async () => {
-                                    try {
-                                        await uploadFile(fileName, filePath);
-                                        resolve();
-                                    } catch (retryErr) {
-                                        console.error(retryErr);
-                                        reject(retryErr);
-                                    }
-                                }, retryDelay);
-                            } else {
-                                console.error(err);
-                                reject(err);
-                            }
-                        } else {
-                            console.log(`Successfully uploaded data to ${process.env.MINIO_BUCKET} / ${fileName}`);
-                            resolve();
-                        }
-                    });
-                }, randomDelay);
-            });
+            let randomDelay = Math.floor(Math.random() * (10000 - 0 + 1)) + 0;
+            console.log(`Uploading file ${fileName} to MINIO bucket ${process.env.MINIO_BUCKET} with delay ${randomDelay} ms`);
+            await new Promise(resolve => setTimeout(resolve, randomDelay));
+            try {
+                await this.minioClient.fPutObject(process.env.MINIO_BUCKET, fileName, filePath);
+                console.log(`Successfully uploaded data to ${process.env.MINIO_BUCKET} / ${fileName}`);
+            } catch (err) {
+                if (err.code === 'SlowDown') {
+                    let retryDelay = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000; // Random delay between 5 and 10 seconds
+                    console.warn(`Received SlowDown error. Retrying upload file ${fileName} to MINIO bucket ${process.env.MINIO_BUCKET} after delay ${retryDelay} ms`);
+                    await new Promise(resolve => setTimeout(resolve, retryDelay));
+                    try {
+                        await uploadFile(fileName, filePath);
+                    } catch (retryErr) {
+                        console.error(retryErr);
+                        throw retryErr;
+                    }
+                } else {
+                    console.error(err);
+                    throw err;
+                }
+            }
         }
 
         const uploadVideo = async (dir: string, file: string): Promise<void> => {
@@ -115,25 +107,12 @@ export class MinioFilesService extends FilesService {
 
 
     async isBucketCreated(bucketName: string): Promise<boolean> {
-        return new Promise((resolve, reject) => {
-            this.minioClient.bucketExists(bucketName, (err, exists) => {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(exists);
-            })
-		});
+        return this.minioClient.bucketExists(bucketName);
     }
 
     async createBucket(bucketName: string): Promise<any> {
-        return new Promise((resolve, reject) => {
-            this.minioClient.makeBucket(bucketName, "", (err) => {
-                if (err) {
-                    return reject(err);
-                }
-                console.log("Success in creating MINIO bucket: " + bucketName);
-                return resolve('');
-            })
-		});
+        await this.minioClient.makeBucket(bucketName, "");
+        console.log("Success in creating MINIO bucket: " + bucketName);
+        return "";
     }
 }
