e6f9f6c158b38ee44d82176f709658347f8039f1
ivchicano <ivchicano@gmail.com>
2024-06-05 16:13:23 +0200

    browser-emulator: make be connector wait for localStorage

--- browser-emulator/public/lib/browser-emulator-connector.js
+++ browser-emulator/public/lib/browser-emulator-connector.js
@@ -1,67 +1,87 @@
 class BrowserEmulatorConnector {
-    sendEvent(event, participant, session) {
-        var ITEM_NAME = 'ov-events-config';
-    
-        const url = JSON.parse(window.localStorage.getItem(ITEM_NAME));
+    async getItem(itemName) {
+        try {
+            let webrtcObj = localStorage.getItem(itemName);
+            const maxRetries = 5;
+            const retryCount = 0;
+            const backoffTime = 1000 + Math.floor(Math.random() * 2000);
 
-        if(url) {
-            return new Promise((resolve, reject) => {
-                let eventObj = event;
-                if (typeof eventObj === 'object' && eventObj !== null) {
-                    eventObj['participant'] = participant;
-                    eventObj['session'] = session;
-                    eventObj['timestamp'] = new Date().toISOString();
-                    eventObj = JSON.stringify(eventObj);
-                }
+            while (!webrtcObj && retryCount < maxRetries) {
+                console.warn("BrowserEmulatorConnector item with name" + itemName + " not found in localStorage, retrying in " + backoffTime + "ms...");
+                await new Promise((resolve) => setTimeout(resolve, backoffTime));
+                webrtcObj = localStorage.getItem(itemName);
+            }
 
-                fetch(url.httpEndpoint, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: eventObj
-                })
-                .then(response => {
-                    if (response.ok) {
-                        resolve();
-                    } else {
-                        reject(new Error('Request failed'));
-                    }
-                })
-                .catch(error => reject(error));
-            });
+            if (!webrtcObj) {
+                throw new Error("BrowserEmulatorConnector item with name" + itemName + " not found in localStorage after multiple retries, stats won't be sent.");
+            }
+
+            return webrtcObj;
+        }
+        catch (e) {
+            console.warn("BrowserEmulatorConnector item with name" + itemName + " not accessible in this context, stats won't be sent.");
         }
     }
-    
+
+    sendEvent(event, participant, session) {
+        var ITEM_NAME = 'ov-events-config';
+
+        this.getItem(ITEM_NAME).then((url) => {
+            const urlObj = JSON.parse(url);
+            let eventObj = event;
+            if (typeof eventObj === 'object' && eventObj !== null) {
+                eventObj['participant'] = participant;
+                eventObj['session'] = session;
+                eventObj['timestamp'] = new Date().toISOString();
+                eventObj = JSON.stringify(eventObj);
+            }
+
+            return fetch(urlObj.httpEndpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: eventObj
+            })
+        }).then((response) => {
+            if (response.ok) {
+                console.log("Event sent to browser-emulator");
+            } else {
+                console.error(response);
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
     sendError(err, participant, session) {
         var ITEM_NAME = 'ov-errorlog-config';
-    
-        const url = JSON.parse(window.localStorage.getItem(ITEM_NAME));
-        if (url) {
+
+        this.getItem(ITEM_NAME).then((url) => {
+            const urlObj = JSON.parse(url);
             let eventObj = err;
             if (typeof eventObj === 'object' && eventObj !== null) {
                 eventObj['participant'] = participant;
                 eventObj['session'] = session;
+                eventObj['timestamp'] = new Date().toISOString();
                 eventObj = JSON.stringify(eventObj);
             }
 
-            fetch(url.httpEndpoint, {
+            return fetch(urlObj.httpEndpoint, {
                 method: 'POST',
-                body: eventObj,
                 headers: {
-                    'Content-Type': 'application/json'
-                }
+                    'Content-Type': 'application/json',
+                },
+                body: eventObj
             })
-            .then(response => {
-                if (response.ok) {
-                    console.log("Error sent to browser-emulator");
-                } else {
-                    console.error(response);
-                }
-            })
-            .catch(error => console.error(error));
-        } else {
-            console.error("No URL in localStorage for error Endpoint");
-        }
+        }).then((response) => {
+            if (response.ok) {
+                console.log("Error sent to browser-emulator");
+            } else {
+                console.error(response);
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
     }
 }
