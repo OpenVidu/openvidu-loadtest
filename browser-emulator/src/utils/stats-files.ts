456a34fc70c4480152ec7f898801532d77e6c2e3
ivchicano <ivchicano@gmail.com>
2024-06-12 19:16:26 +0200

    browser-emulator: fix lockfiles not being found

--- browser-emulator/src/utils/stats-files.ts
+++ browser-emulator/src/utils/stats-files.ts
@@ -13,22 +13,10 @@ if (!fs.existsSync(STATS_DIR)) {
 	fs.mkdirSync(STATS_DIR, { recursive: true });
 }
 
-if (!fs.existsSync(LOCKS_DIR)) {
-	fs.mkdirSync(LOCKS_DIR, { recursive: true });
-}
-
 export async function createFileAndLock(user: string, session: string, file: string) {
-	const lockDir = LOCKS_DIR + session + "/" + user + "/";
-	// create lock directory if it doesn't exist with promises
-	await fsp.mkdir(lockDir, { recursive: true });
-	const lockPath = lockDir + file + ".lock";
-	// create lock file if it doesn't exist with promises
-	if (!fs.existsSync(lockPath)) {
-		await fsp.writeFile(lockPath, "");
-	}
 	const fileDir = STATS_DIR + session + "/" + user + "/";
 	const filePath = fileDir + file;
-	const release = await lockfile.lock(lockPath, retryOptions);
+	const release = await lockfile.lock(filePath, retryOptions);
 	console.log("Creating file " + filePath);
 	await fsp.mkdir(fileDir, { recursive: true });
 	if (!fs.existsSync(filePath)) {
@@ -40,39 +28,44 @@ export async function createFileAndLock(user: string, session: string, file: str
 const retryOptions = {
 	retries: 5,
 	factor: 3,
-	minTimeout: 1000,
+	minTimeout: 10000,
 	maxTimeout: 60000,
 	randomize: true,
 };
 
 export async function saveStatsToFile(user: string, session: string, file: string, data: any) {
-	const lockDir = LOCKS_DIR + session + "/" + user + "/";
-	const lockPath = lockDir + file + ".lock";
 	const fileDir = STATS_DIR + session + "/" + user + "/";
 	const filePath = fileDir + file;
-	const release = await lockfile.lock(lockPath, retryOptions);
-	//console.log("Saving stats to file " + filePath);
-	let existingData: any[] = [];
-    try {
-        // Read existing data from the file
-        const fileContent = await fsp.readFile(filePath, 'utf8');
-        existingData = JSON.parse(fileContent);
-		//console.log("Existing data: " + existingData.length)
-    } catch (error) {
-        // If the file doesn't exist or is empty, continue with an empty array
-    }
+	console.log("Saving stats to file " + filePath);
+	try {
+		const release = await lockfile.lock(filePath, retryOptions);
+		//console.log("Saving stats to file " + filePath);
+		let existingData: any[] = [];
+		try {
+			// Read existing data from the file
+			const fileContent = await fsp.readFile(filePath, 'utf8');
+			existingData = JSON.parse(fileContent);
+			//console.log("Existing data: " + existingData.length)
+		} catch (error) {
+			// If the file doesn't exist or is empty, continue with an empty array
+		}
 
-    // Merge existing data with new data
-	let combinedData: any[];
-	if (Array.isArray(data)) {
-		combinedData = existingData.concat(data);
-	} else {
-		existingData.push(data);
-		combinedData = existingData;
+		// Merge existing data with new data
+		let combinedData: any[];
+		if (Array.isArray(data)) {
+			combinedData = existingData.concat(data);
+		} else {
+			existingData.push(data);
+			combinedData = existingData;
+		}
+		//console.log("Combined data: " + combinedData.length);
+
+		// Write the combined data back to the file
+		await fsp.writeFile(filePath, JSON.stringify(combinedData));
+		console.log("Stats saved to file " + filePath);
+		await release();
+		console.log("Stats released lock " + filePath);
+	} catch (error) {
+		console.error(error);
 	}
-	//console.log("Combined data: " + combinedData.length);
-
-    // Write the combined data back to the file
-    await fsp.writeFile(filePath, JSON.stringify(combinedData));
-	await release();
 }
