895ef1c3315f24683c725c885d69340c0b4b7d8a
ivchicano <ivchicano@gmail.com>
2024-08-12 15:18:53 +0200

    browser-emulator: increase the size of requests to avoid PayloadTooLarge when sending webrtc stats

--- browser-emulator/src/server.ts
+++ browser-emulator/src/server.ts
@@ -61,8 +61,8 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/', eventsController);
 app.use('/openvidu-browser', ovBrowserController);
