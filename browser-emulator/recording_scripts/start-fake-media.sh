4c588cbe412cec37a2545a851da2170c531547e7
ivchicano <ivchicano@gmail.com>
2026-02-27 15:59:30 +0100

    log ffmpeg

--- browser-emulator/recording_scripts/start-fake-media.sh
+++ browser-emulator/recording_scripts/start-fake-media.sh
@@ -5,4 +5,4 @@ ffmpeg -y \
     -map 0 \
     -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 \
     -map 1 \
-    -f s16le -ar 48000 -ac 2 -threads 0 /tmp/virtmic > /dev/null 2>&1 < /dev/null
+    -f s16le -ar 48000 -ac 2 -threads 0 /tmp/virtmic > /var/log/ffmpeg.log 2>&1 < /dev/null
