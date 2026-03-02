9d60d8bea3d80944f75873a4ad37e1dec8d87f45
ivchicano <ivchicano@gmail.com>
2026-03-02 21:58:43 +0100

    add missing file from previous commit

--- browser-emulator/src/data/browser-localstorage-configs/openvidu-events-localstorage.ts
+++ browser-emulator/src/data/browser-localstorage-configs/openvidu-events-localstorage.ts
@@ -2,13 +2,13 @@ import type { OpenViduEventsConfig } from '../../types/storage-config.type.ts';
 import { BaseLocalStorageConfig } from './base-localstorage-config.ts';
 
 export class OpenViduEventsLocalStorage extends BaseLocalStorageConfig {
-	constructor() {
-		super('ov-events-config', '/events');
+	constructor(hostname: string) {
+		super('ov-events-config', '/events', hostname);
 	}
 
 	protected buildConfig(): OpenViduEventsConfig {
 		return {
-			httpEndpoint: this.getEndpointUrl(this.endpoint),
+			httpEndpoint: this.getEndpointUrl(),
 		};
 	}
 }
