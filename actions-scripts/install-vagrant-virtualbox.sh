f293dc6ba77a12cec4cc83c6acb9733d57896599
ivchicano <ivchicano@gmail.com>
2026-03-06 16:19:05 +0100

    try workflow again without virtualbox extension pack

--- actions-scripts/install-vagrant-virtualbox.sh
+++ actions-scripts/install-vagrant-virtualbox.sh
@@ -6,5 +6,4 @@ export DEBIAN_FRONTEND=noninteractive
 curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt-get update
-apt-get install -y vagrant virtualbox virtualbox-ext-pack
-modprobe vboxdrv
+apt-get install -y vagrant virtualbox
