ab13088c0f8779f7ae832c07626640a0da95f528
ivchicano <ivchicano@gmail.com>
2026-03-06 16:38:29 +0100

    another try to installing virtualbox-ext-pack in CI

--- actions-scripts/install-vagrant-virtualbox.sh
+++ actions-scripts/install-vagrant-virtualbox.sh
@@ -6,4 +6,6 @@ export DEBIAN_FRONTEND=noninteractive
 curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor --batch --yes -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt-get update
-apt-get install -y vagrant virtualbox
+echo virtualbox-ext-pack virtualbox-ext-pack/license select true | debconf-set-selections
+echo virtualbox-ext-pack virtualbox-ext-pack/license seen true | debconf-set-selections
+apt-get install -y vagrant virtualbox virtualbox-ext-pack
