9e4c415ac982362eba049b2453dac1d24e8a3fdd
ivchicano <ivchicano@gmail.com>
2026-03-06 16:09:57 +0100

    fix issue installing virtualbox extension pack in CI

--- actions-scripts/install-vagrant-virtualbox.sh
+++ actions-scripts/install-vagrant-virtualbox.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
+export DEBIAN_FRONTEND=noninteractive
+
 curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt-get update
 apt-get install -y vagrant virtualbox virtualbox-ext-pack
+modprobe vboxdrv
