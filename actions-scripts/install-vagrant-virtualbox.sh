fc5d8d97a9b3029f1c57aaf7a86c268ba99a0479
ivchicano <ivchicano@gmail.com>
2026-03-06 12:13:47 +0100

    remove KVM module unloading for workflow

--- actions-scripts/install-vagrant-virtualbox.sh
+++ actions-scripts/install-vagrant-virtualbox.sh
@@ -5,9 +5,3 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt-get update
 apt-get install -y vagrant virtualbox
-
-# Unload KVM modules to avoid conflict with VirtualBox AMD-V/VT-x extension.
-# See: https://github.com/actions/runner-images/issues/13202
-modprobe -r kvm_amd 2>/dev/null || true
-modprobe -r kvm_intel 2>/dev/null || true
-modprobe -r kvm 2>/dev/null || true
