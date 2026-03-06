9317b38aca41677341f94dc270e8d8bdf8f4dd05
ivchicano <ivchicano@gmail.com>
2026-03-06 16:21:25 +0100

    fix workflow gpg asking for interactive input

--- actions-scripts/install-vagrant-virtualbox.sh
+++ actions-scripts/install-vagrant-virtualbox.sh
@@ -3,7 +3,7 @@ set -euox pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor --batch --yes -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt-get update
 apt-get install -y vagrant virtualbox
