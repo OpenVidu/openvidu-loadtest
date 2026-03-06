f7c5df8989764699db25d4b40657e54db96d17b6
ivchicano <ivchicano@gmail.com>
2026-03-06 17:47:31 +0100

    remove DEBIAN_FRONTEND=noninteractive in CI as it causes issues

--- actions-scripts/install-vagrant-virtualbox.sh
+++ actions-scripts/install-vagrant-virtualbox.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-export DEBIAN_FRONTEND=noninteractive
-
 curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor --batch --yes -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor --batch --yes -o /usr/share/keyrings/oracle-virtualbox-2016.gpg
