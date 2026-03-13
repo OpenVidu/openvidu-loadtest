243923fc834a3c5fbb2afb94278adaa1e9b31b41
ivchicano <ivchicano@gmail.com>
2026-03-13 15:57:36 +0100

    browser-emulator: fix PESQ and VISQOL installation

--- browser-emulator/prepare_scripts/install_qoe.sh
+++ browser-emulator/prepare_scripts/install_qoe.sh
@@ -21,16 +21,17 @@ fi
 SELF_PATH="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/../.." && pwd -P)" # Absolute canonical path
 
 ## Install necessary packages
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel.gpg
-mv bazel.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 apt-get update
-apt-get install -yq --no-install-recommends build-essential bc make cmake libopencv-dev python3-opencv bazel libnetpbm10-dev xxd \
+apt-get install -yq --no-install-recommends build-essential bc make cmake libopencv-dev python3-opencv libnetpbm10-dev xxd \
     libjpeg-turbo-progs imagemagick-6.q16 jq automake g++ libtool libleptonica-dev pkg-config nasm ninja-build \
-    meson doxygen libx264-dev libx265-dev libnuma-dev
+    meson doxygen libx264-dev libx265-dev libnuma-dev python3-venv python3-numpy
 pkg-config --cflags --libs opencv4
-# Needed for VISQOL, put here so it doesn't conflict with other python installation parallel to it
-pip3 install --break-system-packages numpy
+# Create a python virtual environment usable by any user
+# Add venv activation to /etc/profile.d so it's loaded for all users by default
+python3 -m venv /opt/venv --system-site-packages
+chmod -R 755 /opt/venv
+echo 'export PATH=/opt/venv/bin:$PATH' | tee -a /etc/profile.d/venv.sh
+export PATH=/opt/venv/bin:$PATH
 
 install_vmaf() {
     ## Install VMAF
@@ -52,8 +53,6 @@ install_vmaf() {
     cd ..
     rm -rf vmaf-2.3.0
     rm vmaf.tar.gz
-    export VMAF_PATH=/usr/local/bin
-    echo export VMAF_PATH=/usr/local/bin | tee -a /etc/profile
 }
 
 install_vqmt() {
@@ -64,38 +63,57 @@ install_vqmt() {
     mv ./build/bin/Release/vqmt /usr/local/bin/vqmt
     cd ..
     rm -rf VQMT
-    export VQMT_PATH=/usr/local/bin
-    echo export VQMT_PATH=/usr/local/bin | tee -a /etc/profile
-    cd $SELF_PATH
+    cd "$SELF_PATH"
 }
 
 install_pesq() {
     ## Install PESQ
     git clone https://github.com/dennisguse/ITU-T_pesq
     cd ITU-T_pesq
-    make
+    # Newer GCC defaults to -fno-common which treats common symbols as multiple
+    # definitions. Build with -fcommon to allow legacy common symbol definitions
+    # present in this upstream code.
+    make CFLAGS="-fcommon"
     mv ./bin/itu-t-pesq2005 /usr/local/bin/pesq
     cd ..
     rm -rf ITU-T_pesq
-    export PESQ_PATH=/usr/local/bin
-    echo export PESQ_PATH=/usr/local/bin | tee -a /etc/profile
 }
 
 install_visqol() {
     ## Install VISQOL
-    curl --output "/tmp/visqol.tar.gz" \
-        --continue-at - \
-        --location "https://github.com/google/visqol/archive/refs/tags/v3.3.3.tar.gz"
-    cd /tmp
-    tar -xvf visqol.tar.gz
-    rm visqol.tar.gz
-    cd visqol-3.3.3
-    bazel build :visqol -c opt
+    # Based on https://github.com/google/visqol/issues/136#issuecomment-4007213267
+    # 0. Install bazelisk
+    curl -L -o /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/v1.28.1/bazelisk-linux-amd64
+    chmod +x /usr/local/bin/bazel
+
+    # 1. First, perform the fetch with the experimental flag.
+    # This downloads the dependencies into the cache.
+    git clone https://github.com/google/visqol.git
+    cd visqol
+    bazel fetch --experimental_repo_remote_exec :visqol
+
+    # 2. Now that the files are downloaded, find and patch spectrogram.cc
+    TFLITE_FILE=$(find $(bazel info output_base)/external -name spectrogram.cc | grep "internal/spectrogram.cc" | head -n 1)
+
+    if [ -n "$TFLITE_FILE" ]; then
+        echo "Patching $TFLITE_FILE..."
+        # Add the missing header required for GCC 11+
+        sed -i '1i #include <cstdint>' "$TFLITE_FILE"
+    else
+        echo "Error: spectrogram.cc not found. Check if the fetch step failed."
+    fi
+
+    # 3. Finally, run the build
+    bazel build :visqol -c opt --copt=-w --experimental_repo_remote_exec
+
+    cp $(bazel info bazel-bin)/visqol /usr/local/bin/visqol
+    chmod 755 /usr/local/bin/visqol
+
+    mkdir -p /usr/local/share/visqol/
+    cp model/libsvm_nu_svr_model.txt /usr/local/share/visqol/libsvm_nu_svr_model.txt
     cd ..
-    mv visqol-3.3.3 /usr/local/visqol
-    export VISQOL_PATH=/usr/local/visqol
-    echo export VISQOL_PATH=/usr/local/visqol | tee -a /etc/profile
-    rm -rf visqol-3.3.3
+    rm -rf visqol
+
     cd $SELF_PATH
 }
 
@@ -125,7 +143,7 @@ install_tesseract() {
 
 install_python_dependencies() {
     ## Install python dependencies
-    pip3 install --break-system-packages -r /opt/openvidu-loadtest/browser-emulator/qoe_scripts/requirements.txt
+    pip3 install -r /opt/openvidu-loadtest/browser-emulator/qoe_scripts/requirements.txt
 }
 
 install_vqmt &
--- browser-emulator/qoe_scripts/analysis_tasks.py
+++ browser-emulator/qoe_scripts/analysis_tasks.py
@@ -1,52 +1,34 @@
 import subprocess as sp
 import os
+import shutil
 import csv
 import math
 import logging as logger
 import ray
 
-vmaf_path = None
-vqmt_path = None
-pesq_path = None
-visqol_path = None
+# Binaries are expected on PATH; we don't store their absolute paths.
+
 
 def validate_install(all_analysis):
-    global vmaf_path
-    global visqol_path
-    vmaf_path = os.environ.get('VMAF_PATH')
-    if not vmaf_path:
-        logger.error("VMAF_PATH environment variable not set")
-        raise Exception("VMAF_PATH environment variable not set")
-    vmaf_path = os.path.join(vmaf_path, 'vmaf')
-    if not os.path.exists(vmaf_path):
-        logger.error("VMAF not found in VMAF_PATH")
-        raise Exception("VMAF not found in VMAF_PATH")
-    visqol_path = os.environ.get('VISQOL_PATH')
-    if not visqol_path:
-        logger.error("VISQOL_PATH environment variable not set")
-        raise Exception("VISQOL_PATH environment variable not set")
-    if not os.path.exists(os.path.join(visqol_path, 'bazel-bin/visqol')):
-        logger.error("VISQOL not found in VISQOL_PATH")
-        raise Exception("VISQOL not found in VISQOL_PATH")
+    # Verify required binaries are available on PATH
+    vmaf_bin = shutil.which("vmaf")
+    if not vmaf_bin:
+        logger.error("vmaf binary not found on PATH")
+        raise Exception("vmaf binary not found on PATH")
+    visqol_bin = shutil.which("visqol")
+    if not visqol_bin:
+        logger.error("visqol binary not found on PATH")
+        raise Exception("visqol binary not found on PATH")
     if all_analysis:
-        global vqmt_path
-        global pesq_path
-        vqmt_path = os.environ.get('VQMT_PATH')
-        if not vqmt_path:
-            logger.error("VQMT_PATH environment variable not set")
-            raise Exception("VQMT_PATH environment variable not set")
-        vqmt_path = os.path.join(vqmt_path, 'vqmt')
-        if not os.path.exists(vqmt_path):
-            logger.error("VQMT not found in VQMT_PATH")
-            raise Exception("VQMT not found in VQMT_PATH")
-        pesq_path = os.environ.get('PESQ_PATH')
-        if not pesq_path:
-            logger.error("PESQ_PATH environment variable not set")
-            raise Exception("PESQ_PATH environment variable not set")
-        pesq_path = os.path.join(pesq_path, 'pesq')
-        if not os.path.exists(pesq_path):
-            logger.error("PESQ not found in PESQ_PATH")
-            raise Exception("PESQ not found in PESQ_PATH")
+        vqmt_bin = shutil.which("vqmt")
+        if not vqmt_bin:
+            logger.error("vqmt binary not found on PATH")
+            raise Exception("vqmt binary not found on PATH")
+        pesq_bin = shutil.which("pesq")
+        if not pesq_bin:
+            logger.error("pesq binary not found on PATH")
+            raise Exception("pesq binary not found on PATH")
+
 
 # TODO: Use ray shared memory instead of files for saving the analysis results then reading them back
 
@@ -83,12 +65,27 @@ def run_vmaf(vid_file, prefix, cut_index, width, height, debug):
     if vid_file == "skip":
         return vid_file
     logger.info("Starting VMAF Analysis of cut %d", cut_index)
-    prefix_with_index = prefix + '-' + str(cut_index)
-    vmaf_command = "%s --threads 1 -p 420 -w %d -h %d -b 8 -r presenter.yuv -d %s -m path=/usr/local/share/vmaf/models/vmaf_v0.6.1.json --json -o $PWD/%s_vmaf.json && cat $PWD/%s_vmaf.json | jq '.frames[].metrics.vmaf' > $PWD/%s_vmaf.csv" % (
-        vmaf_path, width, height, vid_file, prefix_with_index, prefix_with_index, prefix_with_index)
-    return run_analysis_command(vmaf_command, [
-        vid_file, "%s-%d_vmaf.csv" % (prefix, cut_index), "%s-%d_vmaf.json" % (prefix, cut_index)],
-        debug=debug)
+    prefix_with_index = prefix + "-" + str(cut_index)
+    vmaf_command = (
+        "vmaf --threads 1 -p 420 -w %d -h %d -b 8 -r presenter.yuv -d %s -m path=/usr/local/share/vmaf/models/vmaf_v0.6.1.json --json -o $PWD/%s_vmaf.json && cat $PWD/%s_vmaf.json | jq '.frames[].metrics.vmaf' > $PWD/%s_vmaf.csv"
+        % (
+            width,
+            height,
+            vid_file,
+            prefix_with_index,
+            prefix_with_index,
+            prefix_with_index,
+        )
+    )
+    return run_analysis_command(
+        vmaf_command,
+        [
+            vid_file,
+            "%s-%d_vmaf.csv" % (prefix, cut_index),
+            "%s-%d_vmaf.json" % (prefix, cut_index),
+        ],
+        debug=debug,
+    )
 
 
 @ray.remote
@@ -97,40 +94,57 @@ def run_vqmt(vid_file, prefix, cut_index, width, height, debug):
         return vid_file
     logger.basicConfig(level=logger.DEBUG if debug else logger.INFO)
     logger.info("Starting VQMT Analysis of cut %d", cut_index)
-    prefix_with_index = prefix + '-' + str(cut_index)
-    vqmt_command = "%s presenter.yuv %s %d %d 1500 1 %s PSNR SSIM VIFP MSSSIM PSNRHVS PSNRHVSM >> /dev/null 2>&1" % (
-        vqmt_path, vid_file, width, height, prefix_with_index)
-    return run_analysis_command(vqmt_command, [
-        vid_file, "%s-%d_msssim.csv" % (prefix, cut_index),
-        "%s-%d_psnr.csv" % (prefix,
-                            cut_index), "%s-%d_psnrhvs.csv" % (prefix, cut_index),
-        "%s-%d_ssim.csv" % (prefix,
-                            cut_index), "%s-%d_vifp.csv" % (prefix, cut_index),
-        "%s-%d_psnrhvsm.csv" % (prefix, cut_index)], debug=debug)
+    prefix_with_index = prefix + "-" + str(cut_index)
+    vqmt_command = (
+        "vqmt presenter.yuv %s %d %d 1500 1 %s PSNR SSIM VIFP MSSSIM PSNRHVS PSNRHVSM >> /dev/null 2>&1"
+        % (vid_file, width, height, prefix_with_index)
+    )
+    return run_analysis_command(
+        vqmt_command,
+        [
+            vid_file,
+            "%s-%d_msssim.csv" % (prefix, cut_index),
+            "%s-%d_psnr.csv" % (prefix, cut_index),
+            "%s-%d_psnrhvs.csv" % (prefix, cut_index),
+            "%s-%d_ssim.csv" % (prefix, cut_index),
+            "%s-%d_vifp.csv" % (prefix, cut_index),
+            "%s-%d_psnrhvsm.csv" % (prefix, cut_index),
+        ],
+        debug=debug,
+    )
 
 
 @ray.remote
 def run_pesq(audio_files, prefix, cut_index, PESQ_AUDIO_SAMPLE_RATE, debug):
     logger.basicConfig(level=logger.DEBUG if debug else logger.INFO)
     audio_file_pesq = audio_files[1]
-    prefix_with_index = prefix + '-' + str(cut_index)
-    pesq_command = "%s +%s presenter-pesq.wav %s | tail -n 1 > %s_pesq.txt" % (
-        pesq_path, PESQ_AUDIO_SAMPLE_RATE, audio_file_pesq, prefix_with_index)
-    return run_analysis_command(pesq_command, [
-        audio_file_pesq, "%s-%d_pesq.txt" % (prefix, cut_index)],
-        debug=debug)
+    prefix_with_index = prefix + "-" + str(cut_index)
+    pesq_command = "pesq +%s presenter-pesq.wav %s | tail -n 1 > %s_pesq.txt" % (
+        PESQ_AUDIO_SAMPLE_RATE,
+        audio_file_pesq,
+        prefix_with_index,
+    )
+    return run_analysis_command(
+        pesq_command,
+        [audio_file_pesq, "%s-%d_pesq.txt" % (prefix, cut_index)],
+        debug=debug,
+    )
 
 
 @ray.remote
 def run_visqol(audio_files, prefix, cut_index, debug):
     logger.basicConfig(level=logger.DEBUG if debug else logger.INFO)
     audio_file = audio_files[0]
-    prefix_with_index = prefix + '-' + str(cut_index)
-    visqol_command = "%s/bazel-bin/visqol --reference_file presenter.wav --degraded_file %s --verbose --similarity_to_quality_model %s/model/libsvm_nu_svr_model.txt | grep MOS-LQO > %s_visqol.txt" % (
-        visqol_path, audio_file, visqol_path, prefix_with_index)
-    return run_analysis_command(visqol_command, [
-        audio_file, "%s-%d_visqol.txt" % (prefix, cut_index)],
-        debug=debug)
+    prefix_with_index = prefix + "-" + str(cut_index)
+    visqol_command = (
+        "visqol --reference_file presenter.wav --degraded_file %s --verbose --similarity_to_quality_model /usr/local/share/visqol/libsvm_nu_svr_model.txt | grep MOS-LQO > %s_visqol.txt"
+        % (audio_file, prefix_with_index)
+    )
+    return run_analysis_command(
+        visqol_command,
+        [audio_file, "%s-%d_visqol.txt" % (prefix, cut_index)],
+        debug=debug,
+    )
 
 
 def run_analysis_command(command, return_values, debug=False):
@@ -146,15 +160,17 @@ def run_analysis_command(command, return_values, debug=False):
 
 
 def parse_csv(file, column, headers):
-    with open(file, 'r') as f:
+    with open(file, "r") as f:
         reader = csv.reader(f)
         if headers:
             try:
                 next(reader)
             except:
                 logger.warn("No headers found in file %s, it's probably empty", file)
-        results = [float(row[column]) if not math.isnan(
-            float(row[column])) else 0 for row in reader]
+        results = [
+            float(row[column]) if not math.isnan(float(row[column])) else 0
+            for row in reader
+        ]
         return sum(results) / len(results)
 
 
@@ -180,10 +196,10 @@ def parse_vqmt(analysis_results):
 @ray.remote
 def parse_pesq(analysis_results):
     file = analysis_results[1]
-    with open(file, 'r') as f:
+    with open(file, "r") as f:
         text = f.read()
         try:
-            if text == '':
+            if text == "":
                 return 0, [file]
             first_split = text.split("\t")
             rawMOS = float(first_split[0].split("= ")[1])
@@ -196,9 +212,9 @@ def parse_pesq(analysis_results):
 @ray.remote
 def parse_visqol(analysis_results):
     file = analysis_results[1]
-    with open(file, 'r') as f:
+    with open(file, "r") as f:
         text = f.read()
-        if text == '':
+        if text == "":
             return 0, [file]
         try:
             return (float(text.split("MOS-LQO:		")[1])), [file]
