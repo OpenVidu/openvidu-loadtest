9fbd5799be61bc168c59738f3f943282bf1553f2
ivchicano <ivchicano@gmail.com>
2026-03-24 10:42:16 +0100

    browser-emulator: simpify notify aws
    cfn-signal will always be installed and run as sudo

--- browser-emulator/notify-aws.sh
+++ browser-emulator/notify-aws.sh
@@ -10,16 +10,6 @@ set -o xtrace
 pip3 install --break-system-packages https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-latest.tar.gz
 
 # sending the finish call
-CFN_SIGNAL_BIN="$(command -v cfn-signal || true)"
-if [[ -z "${CFN_SIGNAL_BIN}" && -x "/home/ubuntu/.local/bin/cfn-signal" ]]; then
-    CFN_SIGNAL_BIN="/home/ubuntu/.local/bin/cfn-signal"
-fi
-
-if [[ -z "${CFN_SIGNAL_BIN}" ]]; then
-    echo "cfn-signal binary not found" >&2
-    exit 1
-fi
-
-"${CFN_SIGNAL_BIN}" -e 0 --stack "${AWS_STACK}" --resource BrowserInstance --region "${AWS_REGION}"
+cfn-signal -e 0 --stack ${AWS_STACK} --resource BrowserInstance --region ${AWS_REGION}
 
 echo "Instance is ready"
