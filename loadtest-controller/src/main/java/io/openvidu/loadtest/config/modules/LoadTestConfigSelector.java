e5c4f838e849b9b1e4b7a1ee2d195075ba73e154
ivchicano <ivchicano@gmail.com>
2026-04-04 19:26:24 +0200

    loadtest-controller: restore ov2 testing as a hidden config property

--- loadtest-controller/src/main/java/io/openvidu/loadtest/config/modules/LoadTestConfigSelector.java
+++ loadtest-controller/src/main/java/io/openvidu/loadtest/config/modules/LoadTestConfigSelector.java
@@ -14,10 +14,21 @@ public class LoadTestConfigSelector {
     @Bean
     LoadTestConfig loadTestConfig(Environment env) {
         YamlConfigLoader yamlConfig = new YamlConfigLoader(env);
-        
+
+        String platformType = yamlConfig.getString("platform.type").toLowerCase();
+
+        if ("openvidu2".equals(platformType)) {
+            String openviduSecret = yamlConfig.getString("platform.openviduSecret");
+            if (!StringUtils.hasText(openviduSecret)) {
+                throw new IllegalStateException(
+                        "Unable to create OVLoadTestConfig. Define platform.openviduSecret in config.yaml.");
+            }
+            return new OVLoadTestConfig(env);
+        }
+
         String apiKey = yamlConfig.getString("platform.apiKey");
         String apiSecret = yamlConfig.getString("platform.apiSecret");
-        
+
         if (StringUtils.hasText(apiKey) && StringUtils.hasText(apiSecret)) {
             return new LKLoadTestConfig(env);
         }
--- loadtest-controller/src/main/java/io/openvidu/loadtest/models/testcase/request/CreateUserRequestBodyFactory.java
+++ loadtest-controller/src/main/java/io/openvidu/loadtest/models/testcase/request/CreateUserRequestBodyFactory.java
@@ -2,9 +2,11 @@ package io.openvidu.loadtest.models.testcase.request;
 
 import io.openvidu.loadtest.config.LoadTestConfig;
 import io.openvidu.loadtest.config.modules.LKLoadTestConfig;
+import io.openvidu.loadtest.config.modules.OVLoadTestConfig;
 import io.openvidu.loadtest.models.testcase.Role;
 import io.openvidu.loadtest.models.testcase.TestCase;
 import io.openvidu.loadtest.models.testcase.request.modules.LKCreateUserRequestBody;
+import io.openvidu.loadtest.models.testcase.request.modules.OVCreateUserRequestBody;
 
 public class CreateUserRequestBodyFactory {
     private CreateUserRequestBodyFactory() {
@@ -16,6 +18,9 @@ public class CreateUserRequestBodyFactory {
         if (config instanceof LKLoadTestConfig lkConfig) {
             return new LKCreateUserRequestBody(lkConfig, testCase, video, audio, role, userId,
                     sessionId);
+        } else if (config instanceof OVLoadTestConfig ovConfig) {
+            return new OVCreateUserRequestBody(ovConfig, testCase,
+                    video, audio, role, userId, sessionId);
         }
         throw new IllegalArgumentException(
                 "Unable to create request body. Please check your configuration.");
